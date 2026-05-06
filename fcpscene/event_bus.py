6cebaefec979c3ef69b7b1610ccc8fc7d262f3cd
Eric Fortis <efortis@gmail.com>
2026-05-05 23:25:57 -0400

    refactor

--- fcpscene/event_bus.py
+++ fcpscene/event_bus.py
@@ -15,30 +15,30 @@ class EventBus:
 
 
   def emit_progress(self, *args):
-    self._emit('detect.progress', *args)
+    self._emit('DETECT.progress', *args)
 
   def subscribe_progress(self, callback):
-    self._subscribe('detect.progress', callback)
+    self._subscribe('DETECT.progress', callback)
 
   def unsubscribe_progress(self):
-    self._unsubscribe_all('detect.progress')
-
-
-  def emit_export_progress(self, *args):
-    self._emit('export.progress', *args)
-
-  def subscribe_export_progress(self, callback):
-    self._subscribe('export.progress', callback)
-
-  def unsubscribe_export_progress(self):
-    self._unsubscribe_all('export.progress')
+    self._unsubscribe_all('DETECT.progress')
 
 
   def emit_stop(self):
-    self._emit('ffmpeg.stop')
+    self._emit('DETECT.stop')
 
   def subscribe_stop(self, callback):
-    self._subscribe('ffmpeg.stop', callback)
+    self._subscribe('DETECT.stop', callback)
 
   def unsubscribe_stop(self):
-    self._unsubscribe_all('ffmpeg.stop')
+    self._unsubscribe_all('DETECT.stop')
+
+
+  def emit_export_progress(self, *args):
+    self._emit('EXPORT.progress', *args)
+
+  def subscribe_export_progress(self, callback):
+    self._subscribe('EXPORT.progress', callback)
+
+  def unsubscribe_export_progress(self):
+    self._unsubscribe_all('EXPORT.progress')
